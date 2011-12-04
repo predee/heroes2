e7518916e4570c9c3a348b2ab6cdf897d7d1e022
Alexander Poluektov <alexander.poluektov@gmail.com>
2011-12-04 23:24:04 +0100

    Initial commit: main sub, readme, license, cabal.

---
+++ Setup.hs
@@ -0,0 +1,7 @@
+-- Copyright (c) 2011 Alexander Poluektov (alexander.poluektov@gmail.com)
+--
+-- Use, modification and distribution are subject to the MIT license
+-- (See accompanying file MIT-LICENSE)
+
+import Distribution.Simple
+main = defaultMain
---
+++ src/Main.hs
@@ -0,0 +1,6 @@
+-- Copyright (c) 2011 Alexander Poluektov (alexander.poluektov@gmail.com)
+--
+-- Use, modification and distribution are subject to the MIT license
+-- (See accompanying file MIT-LICENSE)
+
+main = putStrLn "Hello, Heroes II World!"
